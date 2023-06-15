04dfc6e00ee49e22b4ab56072517023897b5fa23
Sandra G <sandyjtech@gmail.com>
2023-06-15 06:28:29 -0700

    added DOM Content Loaded

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("text").innerHTML =
+    "JavaScript is so cool. It lets me add text to my page programmatically.";
+});
